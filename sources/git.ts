592206f8610182a2cc8ae24d2e506e0119806f7c
piquark6046 <j.w.so@adguard.com>
2024-07-24 05:18:07 +0000

    chore: enable logging in PushTokenToRepo function

--- sources/git.ts
+++ sources/git.ts
@@ -2,7 +2,7 @@ import * as GitHub from '@octokit/rest'
 
 export async function PushTokenToRepo(Repo: string, Token: string, SHA: string, GitHubToken: string) {
   const GitHubInstance = new GitHub.Octokit({ auth: GitHubToken })
-  await GitHubInstance.actions.createWorkflowDispatch({
+  const Result = await GitHubInstance.actions.createWorkflowDispatch({
     owner: Repo.split('/')[0],
     repo: Repo.split('/')[1],
     workflow_id: '.github/workflows/commit.yml',
@@ -12,4 +12,5 @@ export async function PushTokenToRepo(Repo: string, Token: string, SHA: string, 
       sha: SHA
     }
   })
+  console.log(SHA, Result.status)
 }
